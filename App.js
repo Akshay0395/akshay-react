21d4ad5814064a7e3a5fca255af9a7cfaadf5aad
Akshay Srivastava <akshay.srivastava@vfirst.com>
2023-07-19 17:41:44 +0530

    Push

--- App.js
+++ App.js
@@ -1,65 +1,37 @@
-// const heading = React.createElement("h1",
-// {id:"heading", xyz:"abc"},
-// "Hello World from React!");  //{} is place we give attibute to the tag
-// // React.createElement -> returns js object
-// // h1 -> tag, {} -> attributes(comma seperated for multiple attribute), "" -> text
-// // Third argument in React.createElement is third child we can use that to pass message or to make nested tags and to pass multiple tags then pass inside [] brackets.
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-// //root.render -> converts the object and render the tag in html
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/*
- *
- *   <div id="parent">
- *       <div id="child">
- *           <h1>Hello Akshay</h1>
- *           <h2>Hello Akshay</h2>
- *       </div>
- *   </div>
- *
- */
+const Title = function () {
+  //Normal function
+  return (
+    <h1 id="heading" className="head">
+      Title Component
+    </h1>
+  );
+};
+const elem = <span>React element</span>;
+const title = <h2>Titel Two {elem}</h2>;
+const number = 10000;
+const HeadingComponet = () => (
+  //Arrow function
 
-// const child_three = React.createElement("h2",{},"I'm h2 tag");
-// const child_two = React.createElement("h1",{},"Hello Akshay");
-// const child_one = React.createElement("div",{id:"child"},[child_two,child_three]);
-// const parent = React.createElement("div",{id:"parent"},child_one);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-/*
- *
- *   <div id="parent_one">
- *       <div id="child_one">
- *           <h1>Hello Parent One</h1>
- *           <h2>Hello Child One</h2>
- *       </div>
- *   </div>
- *   <div id="parent_two">
- *       <div id="child_two">
- *           <h1>Hello Parent Two</h1>
- *           <h2>Hello Child Two</h2>
- *       </div>
- *   </div>
- *
- */
-
-const parent_one = React.createElement("div", { id: "parent_one" }, [
-  React.createElement("div", { id: "child_one" }, [
-    React.createElement("h1", {}, "Hello Parent One"),
-    React.createElement("h2", {}, "Hello Child One"),
-  ]),
-  React.createElement("div", { id: "child_two" }, [
-    React.createElement("h1", {}, "Hello Parent One"),
-    React.createElement("h2", {}, "Hello Child Two"),
-  ]),
-]);
-const parent_two = React.createElement(
-  "div",
-  { id: "parent_two" },
-  React.createElement("div", { id: "child_two" }, [
-    React.createElement("h1", {}, "Hello Parent Two"),
-    React.createElement("h2", {}, "Hello Child Two"),
-  ])
+  // React.Fragment => <></>
+  // <React.Fragment>
+  <>
+    <div id="container">
+      <Title />
+      {/* <Title></Title> */}
+      {/* {Title()} //we can call function */}
+      {title}
+      <h1 id="body">React component using function component</h1>
+      <h3>Number One: {number}</h3>
+      <h3>Number Two: {100 * 200}</h3>
+      <h3>Console Log: (See in console) {console.log(number)}</h3>
+    </div>
+  </>
+  // </React.Fragment>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([parent_one, parent_two]);
+root.render(<HeadingComponet />);
+// root.render(title);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR- Hot Module Replacement (When we change any content it automatically update the DOM)
+- File watching algorithm - written in c++ (HMR uses this)
+- Faster build because of caching
+- Image Optimization
+- Mininfication
+- Bundling
+- Compress the file
+- Consistent Hasing
+- Code Spliting
+- Differential Bundling - give the ablibity to support different browser and older version browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Dev and Prod build
